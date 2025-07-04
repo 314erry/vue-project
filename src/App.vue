3d7337c0729ce2ee6e7ff05bd470cbc751fc1268
314erry <314pierry@gmail.com>
2025-07-04 12:35:01 -0300

    Added dynamic listing of jobs from a Json file

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
     <Navbar/>
     <Hero/>
     <HomeCard/>
+    <JobListings/>
 </template>
 
 <script>
     import Navbar from './components/Navbar.vue';
     import Hero from './components/Hero.vue';
     import HomeCard from './components/HomeCard.vue';
+import JobListings from './components/JobListings.vue';
 
     export default {
         components: {
             Navbar,
             Hero,
-            HomeCard
+            HomeCard,
+            JobListings
         }
     }
 </script>
